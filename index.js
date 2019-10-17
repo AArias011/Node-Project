7f763bb0ded00148d0e77ba33611ce5250413923
Anthony Arias <aarias011@west-mec.org>
2019-10-17 15:14:12 -0700

    Added styles to my 5 pages:
    Added 5 more pages and styled all of them, just need to change the color of some.

--- index.js
+++ index.js
@@ -28,10 +28,38 @@ const get_data = async (url,templateNameString, res) => {
          *  2. how can we write this as a call back in our
          */
         res.render(templateNameString, {
+            //Michael
             michaelIMG: json.data[0].avatar,
             firstNameM: json.data[0].first_name,
             lastNameM: json.data[0].last_name,
-            emailM: json.data[0].email
+            emailM: json.data[0].email,
+            idM: json.data[0].id,
+            //Lindsay
+           lindsayIMG: json.data[1].avatar,
+            firstNameL: json.data[1].first_name,
+            lastNameL: json.data[1].last_name,
+            emailL: json.data[1].email,
+            idL: json.data[1].id,
+            //Tobias
+            tobiasIMG: json.data[2].avatar,
+            firstNameT: json.data[2].first_name,
+            lastNameT: json.data[2].last_name,
+            emailT: json.data[2].email,
+            idT: json.data[2].id,
+            //Byron
+            byronIMG: json.data[3].avatar,
+            firstNameB: json.data[3].first_name,
+            lastNameB: json.data[3].last_name,
+            emailB: json.data[3].email,
+            idB: json.data[3].id,
+            //George
+            georgeIMG: json.data[4].avatar,
+            firstNameG: json.data[4].first_name,
+            lastNameG: json.data[4].last_name,
+            emailG: json.data[4].email,
+            idG: json.data[4].id,
+            
+            
         });
 
         // End expressJS code
@@ -42,12 +70,24 @@ const get_data = async (url,templateNameString, res) => {
     }
 };
 
-app.get('/people', (req, res) => {
-    get_data("https://reqres.in/api/users?page=2", "people", res)
+app.get('/lindsay', (req, res) => {
+    get_data("https://reqres.in/api/users?page=2", "lindsay", res)
 });
 
 app.get('/michael', (req, res) => {
     get_data("https://reqres.in/api/users?page=2", "michael" ,res)
+})
+
+app.get('/tobias', (req, res) => {
+    get_data("https://reqres.in/api/users?page=2", "tobias", res)
+})
+
+app.get('/byron', (req, res) => {
+    get_data("https://reqres.in/api/users?page=2", "byron", res)
+})
+
+app.get('/george', (req, res) => {
+    get_data("https://reqres.in/api/users?page=2", "george", res)
 })
 //Routing
 // app.get('/', (req, res) => {
