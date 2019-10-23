68f7091cb1f89b06100527dc2b36ad8269f206ce
Anthony Arias <aarias011@west-mec.org>
2019-10-22 18:26:31 -0700

    Added styles to my footer:
    Added styles to my page.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const get_data = async (url,templateNameString, res) => {
         const response = await fetch(url);
         const json = await response.json();
 
-        /**
+        /*
          *  1.lookup res.render() in express api docs
          *  2. how can we write this as a call back in our
          */
@@ -59,8 +59,6 @@ const get_data = async (url,templateNameString, res) => {
             lastNameG: json.data[4].last_name,
             emailG: json.data[4].email,
             idG: json.data[4].id,
-            
-            
         });
 
         // End expressJS code
@@ -91,54 +89,3 @@ app.get('/george', (req, res) => {
     get_data("https://reqres.in/api/users?page=2", "george", res)
 })
 //Routing
-// app.get('/', (req, res) => {
-    
-//     res.render('index', { 
-//         title:'Home Page', 
-//         name : 'Anthony Arias',
-//     age: '5',
-//     isDisplayName: true,
-//     isAgeEnabled: true
-// });
-// });
-
-// app.get('/contact', (req, res) => {
-
-// fetch('https://reqres.in/api/users?page=2')
-// .then(response => response.json())
-// .then(data => {
-//     console.log(data)
-
-
-//     // app.use(express.static())
-// res.render('contact', {
-//     });
-
-// });
-
-    
-// })
-// app.get('/about', (req, res) => {
-
-
-//     res.render('about', { 
-//     title:'About Me',
-//     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui praesentium iusto eveniet nostrum commodi nobis consectetur velit non cupiditate itaque consequatur magni, quod deserunt hic dolor nam, saepe architecto sed nesciunt, eius labore. Non aut temporibus quae consequatur facilis, vitae deserunt sit iusto cum aliquam. Dignissimos nam ipsum, accusamus dolores sed veritatis natus soluta eius corporis, quae libero rerum cum necessitatibus, officia incidunt. Consectetur numquam assumenda ipsa? Quisquam repudiandae animi quidem asperiores totam illo dolorem in saepe, maiores maxime laboriosam reprehenderit. Omnis quod autem tempore nesciunt sunt ea ullam delectus.'
-// });
-// });
-
-// app.get('/dashboard', (req, res) =>{
-//     res.render('dashboard',{
-//         isListEnabled: false
-//     })
-// })
-
-// app.get('/lookup', (req, res) => {
-//     res.render('lookup', {
-//         user: {
-//             username: 'ntharias',
-//             age:18,
-//             phone:3453453453
-//         },
-//     });
-// })
